6dcc2437c2d69a02f4b1d650f7afac6d3f436224
Dev5611 <vtitanses@ukr.net>
2025-08-31 01:21:54 +0200

    first

--- HW1_1.py
+++ HW1_1.py
@@ -1,8 +1,16 @@
 import math
 
-def calculate_circle_area(radius):
+
+def calculate_circle_area(radius: float) -> float:
+    """
+    Calculate the area of a circle.
+
+    :param radius: Radius of the circle.
+    :return: Area of the circle.
+    """
     return math.pi * radius ** 2
 
-r = float(input("Enter the radius of the circle: "))
-a = calculate_circle_area(r)
-print("The area of the circle is: ", a)
+
+r: float = float(input("Enter the radius of the circle: "))
+a: float = calculate_circle_area(r)
+print("The area of the circle is:", a)
--- HW1_2.py
+++ HW1_2.py
@@ -1,18 +1,35 @@
 class Rectangle:
-    def __init__(self, width, height):
+    """Class representing a rectangle with width and height."""
+
+    def __init__(self, width: float, height: float) -> None:
+        """
+        Initialize a rectangle.
+
+        :param width: Width of the rectangle.
+        :param height: Height of the rectangle.
+        """
         self.width = width
         self.height = height
 
-    def area(self):
+    def area(self) -> float:
+        """Return the area of the rectangle."""
         return self.width * self.height
 
-    def perimeter(self):
+    def perimeter(self) -> float:
+        """Return the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
-    def is_square(self):
+    def is_square(self) -> bool:
+        """Return True if the rectangle is a square, otherwise False."""
         return self.width == self.height
 
-    def resize(self, new_width, new_height):
+    def resize(self, new_width: float, new_height: float) -> None:
+        """
+        Resize the rectangle.
+
+        :param new_width: New width of the rectangle.
+        :param new_height: New height of the rectangle.
+        """
         self.width = new_width
         self.height = new_height
 
@@ -25,3 +42,4 @@ print("Is square?", r.is_square())
 r.resize(6, 6)
 print("New size:", r.width, "x", r.height)
 print("Is square?", r.is_square())
+
