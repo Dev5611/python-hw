93e412839986f7a0b8c155594a79b4b5f691d35f
Dev5611 <vtitanses@ukr.net>
2025-08-31 01:51:26 +0200

    Remove .idea folder

--- HW1_1.py
+++ HW1_1.py
@@ -1,16 +1,18 @@
 import math
-
+import sys
 
 def calculate_circle_area(radius: float) -> float:
     """
-    Calculate the area of a circle.
+    Calculate the area of a circle
 
-    :param radius: Radius of the circle.
-    :return: Area of the circle.
+    :param radius: Radius of the circle
+    :return: Area of the circle
     """
     return math.pi * radius ** 2
 
-
 r: float = float(input("Enter the radius of the circle: "))
+if r < 0:
+    print("Error: Radius cannot be negative.")
+    sys.exit()  #exit
 a: float = calculate_circle_area(r)
 print("The area of the circle is:", a)
--- HW1_2.py
+++ HW1_2.py
@@ -1,34 +1,34 @@
 class Rectangle:
-    """Class representing a rectangle with width and height."""
+    """Class representing a rectangle with width and height"""
 
     def __init__(self, width: float, height: float) -> None:
         """
-        Initialize a rectangle.
+        Initialize a rectangle
 
-        :param width: Width of the rectangle.
-        :param height: Height of the rectangle.
+        :param width: Width of the rectangle
+        :param height: Height of the rectangle
         """
         self.width = width
         self.height = height
 
     def area(self) -> float:
-        """Return the area of the rectangle."""
+        """Return the area of the rectangle"""
         return self.width * self.height
 
     def perimeter(self) -> float:
-        """Return the perimeter of the rectangle."""
+        """Return the perimeter of the rectangle"""
         return 2 * (self.width + self.height)
 
     def is_square(self) -> bool:
-        """Return True if the rectangle is a square, otherwise False."""
+        """Return True if the rectangle is a square, otherwise False"""
         return self.width == self.height
 
     def resize(self, new_width: float, new_height: float) -> None:
         """
-        Resize the rectangle.
+        Resize the rectangle
 
-        :param new_width: New width of the rectangle.
-        :param new_height: New height of the rectangle.
+        :param new_width: New width of the rectangle
+        :param new_height: New height of the rectangle
         """
         self.width = new_width
         self.height = new_height
